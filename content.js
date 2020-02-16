bf5c70d9a19c17c870a6ed4bcb8fb5de5add899e
m.morshinin <m.morshinin@b2b-center.ru>
2020-02-16 22:51:06 +0300

    Initial commit

---
+++ content.js
@@ -0,0 +1,33 @@
+const page_url = document.URL;
+
+function seoAndHeader() {
+    document.querySelector('title').innerText = `${document.querySelector('h1').innerText} - Локально - B2B-Center`;
+    document.querySelector('.slide_down-content .inner .afake').after(document.querySelector('.header-nav-links'));
+    document.querySelector('.header-title-platform').remove();
+    document.querySelector('.header-logo').remove();
+    document.querySelector('.header-services-link > span').innerHTML = '';
+}
+
+function getRidOfAllBullshit() {
+    const user_ico = document.createElement('div');
+    const all_user_shit = document.createElement('div');
+
+    user_ico.setAttribute('class', 'header-user_ico');
+    all_user_shit.setAttribute('class', 'header-all_user_shit');
+
+    const main_row = document.querySelector('.header-main-left');
+    all_user_shit.appendChild(document.querySelector('#ph-user-name-panel'));
+    all_user_shit.appendChild(document.querySelector('#ph-user-panel'));
+    all_user_shit.appendChild(document.querySelector('#ph-header-bottom-panel'));
+
+    main_row.before(all_user_shit);
+    main_row.before(user_ico);
+
+    user_ico.addEventListener('click', () => {
+        all_user_shit.classList.toggle('all_user_shit--active');
+    });
+}
+
+seoAndHeader();
+getRidOfAllBullshit();
+
